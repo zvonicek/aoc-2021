5027bde3dcc61a434aaa414dbbe39ce716af6b57
Petr Zvonicek <petrz@spotify.com>
2021-12-29 22:07:55 +0100

    Add Day 23, part 2

--- src/Day23.kt
+++ src/Day23.kt
@@ -17,148 +17,130 @@ enum class CellType {
 }
 
 data class Configuration(
-    var hallwayIndexes: List<Int>,
-    var room1Indexes: List<Int>,
-    var room2Indexes: List<Int>,
-    var room3Indexes: List<Int>,
-    var room4Indexes: List<Int>
+    var hallwayIndexesSet: Set<Int>,
+    var room1Indexes: IntRange,
+    var room2Indexes: IntRange,
+    var room3Indexes: IntRange,
+    var room4Indexes: IntRange
 ) {
     var roomIndexesSet: Set<Int>
-    var hallwayIndexesSet: Set<Int>
+    var maxHallwayIndex: Int
 
     init {
-        roomIndexesSet = (room1Indexes + room2Indexes + room3Indexes + room4Indexes).toSet()
-        hallwayIndexesSet = hallwayIndexes.toSet()
+        roomIndexesSet = room1Indexes.toSet().union(room2Indexes.toSet()).union(room3Indexes.toSet()).union(room4Indexes.toSet())
+        maxHallwayIndex = hallwayIndexesSet.maxOrNull()!!
+    }
+
+    fun targetField(): Field {
+        val indexes = room1Indexes.map { Pair(it, CellType.A) } + room2Indexes.map { Pair(it, CellType.B) } +
+                room3Indexes.map { Pair(it, CellType.C) } + room4Indexes.map { Pair(it, CellType.D) }
+        return Field(indexes.associate { it.first to it.second })
     }
 }
 
-//class Environment(var config: Configuration) {
-//
-//}
+class Environment(var config: Configuration) {
+    // one of the indices in range is expected to be in the hallway, one in the room
+    fun distance(between: IntRange): Int {
+        val hallwayIndexesBetween = between.subtract(config.roomIndexesSet).count()
+        // line below assumes that all rooms are equally big
+        val roomsBetween = ceil(between.intersect(config.roomIndexesSet).count() / config.room1Indexes.count().toDouble()).toInt()
+        val roomMovements = listOf(config.room1Indexes, config.room2Indexes, config.room3Indexes, config.room4Indexes)
+            .mapNotNull {
+                if (it.contains(between.first)) between.first - it.first
+                else if (it.contains(between.last)) between.last - it.first
+                else null
+            }.first() // assumes that there is one and only one end inside the room
+        return hallwayIndexesBetween + roomsBetween + roomMovements
+    }
 
-// Locations: 0 1 [2 3] 4 [5 6] 7 [8 9] 10 [11 12] 13 14
-val task1config = Configuration(listOf(0, 1, 4, 7, 10, 13, 14), listOf(2, 3), listOf(5, 6), listOf(8, 9), listOf(11, 12))
+    fun isInFinalPosition(locations: Map<Int, CellType>, index: Int): Boolean {
+        val roomIndex = listOf(config.room1Indexes, config.room2Indexes, config.room3Indexes, config.room4Indexes)
+            .first { it.contains(index) } // assumes that rooms indexes are disjunct
 
-val hallwayIndexes = setOf(0, 1, 4, 7, 10, 13, 14)
-val room1Indexes = setOf(2, 3)
-val room2Indexes = setOf(5, 6)
-val room3Indexes = setOf(8, 9)
-val room4Indexes = setOf(11, 12)
-val roomIndexes = room1Indexes.union(room2Indexes).union(room3Indexes).union(room4Indexes)
+        if (roomIndex.last != index && !isInFinalPosition(locations, index+1)) return false
+        return hasValidRoom(index, locations[index]!!)
+    }
 
-// one of the indices in range is expected to be in the hallway, one in the room
-fun distance(between: IntRange): Int {
-    val hallwayIndexesBetween = between.subtract(roomIndexes).count()
-    val roomsBetween = ceil(between.intersect(roomIndexes).count() / 2.0).toInt()
-    val roomMovements = setOf(3,6,9,12).intersect(setOf(between.first, between.last)).count()
-    return hallwayIndexesBetween + roomsBetween + roomMovements
-}
+    fun hasValidRoom(index: Int, cellType: CellType): Boolean {
+        return (config.room1Indexes.contains(index) && cellType == CellType.A) ||
+                (config.room2Indexes.contains(index) && cellType == CellType.B) ||
+                (config.room3Indexes.contains(index) && cellType == CellType.C) ||
+                (config.room4Indexes.contains(index) && cellType == CellType.D)
+    }
 
-fun hasValidRoom(index: Int, cellType: CellType): Boolean {
-    return ((index == 2 || index == 3) && cellType == CellType.A) ||
-            ((index == 5 || index == 6) && cellType == CellType.B) ||
-            ((index == 8 || index == 9) && cellType == CellType.C) ||
-            ((index == 11 || index == 12) && cellType == CellType.D)
-}
+    fun canMoveFromRoom(locations: Map<Int, CellType>, index: Int): Boolean {
+        val roomIndex = listOf(config.room1Indexes, config.room2Indexes, config.room3Indexes, config.room4Indexes)
+            .first { it.contains(index) }
+        val noItemsAbove = if (roomIndex.first == index) true else locations[index-1] == null
+        return locations[index] != null && noItemsAbove && !isInFinalPosition(locations, index)
+    }
 
-data class Field(var locations: Map<Int, CellType>) {
-    fun completeRoomPositions(): Int {
-        return roomIndexes.fold(0) { acc, index ->
-            val lowerPositionComplete = setOf(3, 6, 9, 12).contains(index) && locations[index] != null && hasValidRoom(index, locations[index]!!)
-            val upperPositionComplete =  setOf(2, 5, 8, 11).contains(index) && locations[index] != null && hasValidRoom(index, locations[index]!!) &&
-                    hasValidRoom(index+1, locations[index+1]!!)
+    fun canMoveToRoom(locations: Map<Int, CellType>, index: Int, cellType: CellType): Boolean {
+        val roomIndex = listOf(config.room1Indexes, config.room2Indexes, config.room3Indexes, config.room4Indexes)
+            .first { it.contains(index) }
+        val noItemsAbove = if (roomIndex.first == index) true else locations[index-1] == null
+        val allBelowInFinalPosition = if (roomIndex.last == index) true else locations[index+1] != null && isInFinalPosition(locations, index+1)
+        return hasValidRoom(index, cellType) && noItemsAbove && allBelowInFinalPosition
+    }
 
-            acc + if (lowerPositionComplete || upperPositionComplete) 1 else 0
+    fun nextMovementsFor(field: Field): List<Movement> {
+        val movements = mutableListOf<Movement>()
+
+        for ((key, value) in field.locations) {
+            val hallwayMin = field.locations.keys.filter { config.hallwayIndexesSet.contains(it) && it < key }.maxOrNull()?.plus(1) ?: 0
+            val hallwayMax = field.locations.keys.filter { config.hallwayIndexesSet.contains(it) && it > key }.minOrNull()?.minus(1) ?: config.maxHallwayIndex
+
+            if (config.hallwayIndexesSet.contains(key)) {
+                val emptyRoomIndexes = (hallwayMin..hallwayMax).intersect(config.roomIndexesSet).subtract(field.locations.keys)
+                    .filter { canMoveToRoom(field.locations, it, value) }
+                emptyRoomIndexes.forEach { newIndex ->
+                    val newLocations = field.locations.toMutableMap()
+                    val dist = distance(min(key, newIndex)..max(key, newIndex)) * value.stepCost()
+                    newLocations.remove(key)
+                    newLocations[newIndex] = value
+                    return listOf(Movement(Field(newLocations), dist))
+                }
+            } else if (config.roomIndexesSet.contains(key) && canMoveFromRoom(field.locations, key)) {
+                val emptyHallwayIndexes = (hallwayMin..hallwayMax).intersect(config.hallwayIndexesSet)
+                emptyHallwayIndexes.forEach { newIndex ->
+                    val newLocations = field.locations.toMutableMap()
+                    val dist = distance(min(key, newIndex)..max(key, newIndex)) * value.stepCost()
+                    newLocations.remove(key)
+                    newLocations[newIndex] = value
+                    movements.add(Movement(Field(newLocations), dist))
+                }
+            }
         }
+
+        return movements
     }
 
-    fun canMoveFromRoom(index: Int): Boolean {
-        val validRoom = hasValidRoom(index, locations[index]!!)
-        fun validUpperPosition(position: Int): Boolean = index == position && (!validRoom || !hasValidRoom(position+1, locations[position+1]!!))
-        fun validLowerPosition(position: Int): Boolean = index == position && locations[position-1] == null && !validRoom
+    fun buildFieldStateGraph(initialField: Field): Map<Field, List<Movement>> {
+        val fieldStateGraph = mutableMapOf<Field, List<Movement>>()
+        val fieldStatesToProcess = mutableSetOf<Field>(initialField)
 
-        return validUpperPosition(2) || validLowerPosition(3) ||
-                validUpperPosition(5) || validLowerPosition(6) ||
-                validUpperPosition(8) || validLowerPosition(9) ||
-                validUpperPosition(11) || validLowerPosition(12)
-    }
+        while (fieldStatesToProcess.isNotEmpty()) {
+            val field = fieldStatesToProcess.iterator().next()
+            fieldStatesToProcess.remove(field)
 
-    fun canMoveToRoom(index: Int, cellType: CellType): Boolean {
-        val validRoom = hasValidRoom(index, cellType)
-        val validIndex = index == 3 || (index == 2 && locations[3] != null && hasValidRoom(3, locations[3]!!)) ||
-                index == 6 || (index == 5 && locations[6] != null && hasValidRoom(6, locations[6]!!)) ||
-                index == 9 || (index == 8 && locations[9] != null && hasValidRoom(9, locations[9]!!)) ||
-                index == 12 || (index == 11 && locations[12] != null  && hasValidRoom(12, locations[12]!!))
-        return validIndex && validRoom
+            val movements = nextMovementsFor(field)
+            fieldStateGraph[field] = movements
+
+            val newMovements = movements.map { it.field }.filter { !fieldStateGraph.contains(it) }
+            fieldStatesToProcess.addAll(newMovements)
+        }
+
+        return fieldStateGraph
     }
 }
 
+data class Field(var locations: Map<Int, CellType>)
 data class Movement(var field: Field, var cost: Int)
 
-fun nextMovementsFor(field: Field): List<Movement> {
-    val movements = mutableListOf<Movement>()
-
-    for ((key, value) in field.locations) {
-        val hallwayMin = field.locations.keys.filter { hallwayIndexes.contains(it) && it < key }.maxOrNull()?.plus(1) ?: 0
-        val hallwayMax = field.locations.keys.filter { hallwayIndexes.contains(it) && it > key }.minOrNull()?.minus(1) ?: 14
-
-        if (hallwayIndexes.contains(key)) {
-            val emptyRoomIndexes = (hallwayMin..hallwayMax).intersect(roomIndexes).subtract(field.locations.keys)
-                .filter { field.canMoveToRoom(it, value) }
-            emptyRoomIndexes.forEach { newIndex ->
-                val newLocations = field.locations.toMutableMap()
-                val dist = distance(min(key, newIndex)..max(key, newIndex)) * value.stepCost()
-                newLocations.remove(key)
-                newLocations[newIndex] = value
-                movements.add(Movement(Field(newLocations), dist))
-            }
-        } else if (roomIndexes.contains(key) && field.canMoveFromRoom(key)) {
-            val emptyHallwayIndexes = (hallwayMin..hallwayMax).intersect(hallwayIndexes)
-            emptyHallwayIndexes.forEach { newIndex ->
-                val newLocations = field.locations.toMutableMap()
-                val dist = distance(min(key, newIndex)..max(key, newIndex)) * value.stepCost()
-                newLocations.remove(key)
-                newLocations[newIndex] = value
-                movements.add(Movement(Field(newLocations), dist))
-            }
-        }
-    }
-
-   val maxMovements = movements.fold(listOf<Movement>()) { acc, movement ->
-        val previousMaxComplete = acc.firstOrNull()?.field?.completeRoomPositions() ?: 0
-        if (movement.field.completeRoomPositions() > previousMaxComplete) {
-            listOf(movement)
-        } else {
-            acc + movement
-        }
-    }
-    return maxMovements
-}
-
-fun buildFieldStateGraph(initialField: Field): Map<Field, List<Movement>> {
-    val fieldStateGraph = mutableMapOf<Field, List<Movement>>()
-    val fieldStatesToProcess = mutableSetOf<Field>(initialField)
-
-    while (fieldStatesToProcess.isNotEmpty()) {
-        val field = fieldStatesToProcess.iterator().next()
-        fieldStatesToProcess.remove(field)
-
-        val movements = nextMovementsFor(field)
-        fieldStateGraph[field] = movements
-
-        val newMovements = movements.map { it.field }.filter { !fieldStateGraph.contains(it) }
-        fieldStatesToProcess.addAll(newMovements)
-    }
-
-    return fieldStateGraph
-}
-
-fun dijkstra(start: Field, graph: Map<Field, List<Movement>>): Int {
+fun dijkstra(start: Field, graph: Map<Field, List<Movement>>, target: Field): Int {
     val dist = mutableMapOf<Field, Int>()
     val prev = mutableMapOf<Field, Field>()
     val pQ = PriorityQueue<Field>() { a, b -> dist[a]!! - dist[b]!! }
-    var target = Field(mapOf(Pair(2, CellType.A), Pair(3, CellType.A), Pair(5, CellType.B), Pair(6, CellType.B),
-        Pair(8, CellType.C), Pair(9, CellType.C), Pair(11, CellType.D), Pair(12, CellType.D)))
 
     for (v in graph.keys) {
         dist[v] = Int.MAX_VALUE
@@ -181,6 +163,10 @@ fun dijkstra(start: Field, graph: Map<Field, List<Movement>>): Int {
                 // recompute value `v` in prio queue
                 pQ.remove(v.field)
                 pQ.offer(v.field)
+
+                if (prev.count() % 5000 == 0) {
+                    println("path size: " + prev.count().toString())
+                }
             }
         }
     }
@@ -190,24 +176,46 @@ fun dijkstra(start: Field, graph: Map<Field, List<Movement>>): Int {
 
 fun main() {
     fun part1(field: Field): Int {
-        val graph = buildFieldStateGraph(field)
+        // Locations: 0 1 [2 3] 4 [5 6] 7 [8 9] 10 [11 12] 13 14
+        val cfg = Configuration(setOf(0, 1, 4, 7, 10, 13, 14), 2..3, 5..6, 8..9, 11..12)
+        val env = Environment(cfg)
+
+        val graph = env.buildFieldStateGraph(field)
         println("graph done, nodes:" + graph.keys.count())
-        return dijkstra(field, graph)
+        return dijkstra(field, graph, cfg.targetField())
     }
 
-    fun part2(input: List<String>): Int {
-        return 2
+    fun part2(field: Field): Int {
+        val cfg = Configuration(setOf(0, 1, 6, 11, 16, 21, 22), 2..5, 7..10, 12..15, 17..20)
+        val env = Environment(cfg)
+
+        val graph = env.buildFieldStateGraph(field)
+        println("graph done, nodes:" + graph.keys.count())
+        return dijkstra(field, graph, cfg.targetField())
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testField = Field(mapOf(Pair(2, CellType.B), Pair(3, CellType.A), Pair(5, CellType.C), Pair(6, CellType.D), Pair(8, CellType.B), Pair(9, CellType.C), Pair(11, CellType.D), Pair(12, CellType.A)))
-    //check2(part1(testField), 12521)
-    //check2(part2(testInput), 2)
+    val testField1 = Field(mapOf(Pair(2, CellType.B), Pair(3, CellType.A),
+        Pair(5, CellType.C), Pair(6, CellType.D),
+        Pair(8, CellType.B), Pair(9, CellType.C),
+        Pair(11, CellType.D), Pair(12, CellType.A)))
+    check2(part1(testField1), 12521)
 
-    val field = Field(mapOf(Pair(2, CellType.C), Pair(3, CellType.D), Pair(5, CellType.C), Pair(6, CellType.A), Pair(8, CellType.B), Pair(9, CellType.B), Pair(11, CellType.D), Pair(12, CellType.A)))
-    //check2(part1(input), 15299)
-    //check2(part2(input), 2)
+    val testField2 = Field(mapOf(Pair(2, CellType.B), Pair(3, CellType.D), Pair(4, CellType.D), Pair(5, CellType.A),
+        Pair(7, CellType.C), Pair(8, CellType.C), Pair(9, CellType.B), Pair(10, CellType.D),
+        Pair(12, CellType.B), Pair(13, CellType.B), Pair(14, CellType.A), Pair(15, CellType.C),
+        Pair(17, CellType.D), Pair(18, CellType.A), Pair(19, CellType.C), Pair(20, CellType.A))
+    )
+    check2(part2(testField2), 44169)
 
-    println(part1(field))
-    //println(part2(input))
+    val field1 = Field(mapOf(Pair(2, CellType.C), Pair(3, CellType.D),
+        Pair(5, CellType.C), Pair(6, CellType.A),
+        Pair(8, CellType.B), Pair(9, CellType.B),
+        Pair(11, CellType.D), Pair(12, CellType.A)))
+    check2(part1(field1), 15299)
+
+    val field2 = Field(mapOf(Pair(2, CellType.C), Pair(3, CellType.D), Pair(4, CellType.D), Pair(5, CellType.D),
+        Pair(7, CellType.C),  Pair(8, CellType.C), Pair(9, CellType.B), Pair(10, CellType.A),
+        Pair(12, CellType.B), Pair(13, CellType.B), Pair(14, CellType.A), Pair(15, CellType.B),
+        Pair(17, CellType.D), Pair(18, CellType.A), Pair(19, CellType.C), Pair(20, CellType.A)))
+    check2(part2(field2), 47193)
 }
